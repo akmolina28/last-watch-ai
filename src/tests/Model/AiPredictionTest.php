0f0046a80fc361c8983b470dd1f7966a83583a3f
Andrew Molina <2997206+akmolina28@users.noreply.github.com>
2020-10-14 09:10:20 -0500

    Added php code sniffer. Ran phpcbf.

--- src/tests/Model/AiPredictionTest.php
+++ src/tests/Model/AiPredictionTest.php
@@ -1,14 +1,12 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Model;
 
 use App\AiPrediction;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
-use App\DetectionProfile;
 
 class AiPredictionTest extends TestCase
 {
