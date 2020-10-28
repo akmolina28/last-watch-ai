57562726f544425fd77fdf7190f96a0a29ac3c8c
Andrew Molina <2997206+akmolina28@users.noreply.github.com>
2020-10-27 21:44:35 -0500

    Fixed UTC issue

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -8,11 +8,11 @@ import TitleHeader from './components/TitleHeader';
 Vue.component('title-header', TitleHeader);
 
 Vue.filter('dateStr', function(value) {
-    return moment.utc(value).local();
+    return moment(value).local();
 });
 
 Vue.filter('dateStrRelative', function(value) {
-    return moment.utc(value).fromNow();
+    return moment(value).local().fromNow();
 });
 
 new Vue({
