b7dee7c8ca4f4efeb5ee3d18779e0058d1cca193
Andrew Molina <2997206+akmolina28@users.noreply.github.com>
2020-12-23 10:49:23 -0600

    Refactor and add more substitution variables

---
+++ src/app/PayloadHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App;
+
+
+class PayloadHelper
+{
+    public static function doReplacements(
+        string $payload, DetectionEvent $event, DetectionProfile $profile)
+    {
+
+        $replaced = $payload;
+
+        $replaced = str_replace('%image_file_name%', $event->image_file_name, $replaced);
+
+        $replaced = str_replace('%profile_name%', $profile->name, $replaced);
+
+        $objectClasses = $profile->aiPredictions()
+            ->where('detection_event_id', '=', $event->id)
+            ->where('ai_prediction_detection_profile.is_masked', '=', 0)
+            ->where('ai_prediction_detection_profile.is_smart_filtered', '=', 0)
+            ->pluck('object_class')
+            ->sort()
+            ->implode(',');
+
+        $replaced = str_replace('%object_classes%', $objectClasses, $replaced);
+
+        $replaced = str_replace('%event_url%', url('/events/'.$event->id), $replaced);
+
+        $replaced = str_replace('%image_url%', url('/storage/'.$event->image_file_name), $replaced);
+
+        return $replaced;
+    }
+}
