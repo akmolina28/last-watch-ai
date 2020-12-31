36ae016f7bc75f0ad5ea2b6b2b33e9e9dd6f1a5a
Andrew Molina <2997206+akmolina28@users.noreply.github.com>
2020-12-31 10:31:48 -0600

    Fix intermittent test failures (detection event factory was creating profiles with a min confidence of 100% which then excluded every prediction)

--- src/database/factories/DetectionProfileFactory.php
+++ src/database/factories/DetectionProfileFactory.php
@@ -10,7 +10,7 @@ $factory->define(DetectionProfile::class, function (Faker $faker) {
         'name' => $faker->unique()->words(3, true),
         'file_pattern' => $faker->word(),
         'object_classes' => $faker->randomElements(config('deepstack.object_classes'), $faker->numberBetween(1, 5)),
-        'min_confidence' => $faker->numberBetween(45, 100) / 100,
+        'min_confidence' => $faker->numberBetween(45, 90) / 100,
         'use_regex' => false,
         'use_mask' => $faker->boolean(),
     ];
