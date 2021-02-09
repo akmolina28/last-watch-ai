1ddb5db19dd73ddc028e3f264afd4791c4083e5b
Andrew <akmolina28@users.noreply.github.com>
2021-02-09 16:39:27 +0000

    Apply fixes from StyleCI

--- src/app/PayloadHelper.php
+++ src/app/PayloadHelper.php
@@ -30,7 +30,6 @@ class PayloadHelper
         return $replaced;
     }
 
-
     public static function getEventPayload(DetectionEvent $event, DetectionProfile $profile)
     {
         $predictions = $profile->aiPredictions()
